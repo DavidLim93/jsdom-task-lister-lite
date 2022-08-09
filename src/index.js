365862e2fadcd0691c548097b8d010e59315269c
DavidLim93 <lim.david93@gmail.com>
2022-08-10 08:57:38 +1000

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+function task () {
+const form = document.getElementById("create-task-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const task=document.getElementById("new-task-description").value;
+  const newTask=document.getElementById("tasks");
+  const newList=document.getElementById("li");
+  newTask.innerText= task;
+  newList,this.appendChild(newTask);
+});
+} 
